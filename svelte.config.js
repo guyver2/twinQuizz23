85a41311ba49997e2c532790c314a82d6c4e9293
Antoine Letouzey <antoine.letouzey@gmail.com>
2023-03-10 14:55:29 +0100

    support for https://quizz.sxbn.org

--- svelte.config.js
+++ svelte.config.js
@@ -4,18 +4,19 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true
-		})
-	],
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      postcss: true
+    })
+  ],
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    csrf: {
+      checkOrigin: false,
+    }
+  }
 };
 
 export default config;
